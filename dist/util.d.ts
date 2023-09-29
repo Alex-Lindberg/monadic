47b23d0ab3cffa11255e230aa5bf9025aa472d02
Alex Lindberg <alex.lindberg@nvrmind.se>
2023-09-29 11:17:39 +0200

    Build after eslint

--- dist/util.d.ts
+++ dist/util.d.ts
@@ -1,55 +1,41 @@
 import { Monad } from "./monad";
 declare class Success<T, E> {
-  value: T;
-  constructor(value: T);
-  isSuccess(): this is Success<T, E>;
+    value: T;
+    constructor(value: T);
+    isSuccess(): this is Success<T, E>;
 }
 declare class Failure<T, E> {
-  error: E;
-  constructor(error: E);
-  isSuccess(): this is Success<T, E>;
+    error: E;
+    constructor(error: E);
+    isSuccess(): this is Success<T, E>;
 }
 type Either<T, E> = Success<T, E> | Failure<T, E>;
 type MatchCondition<T, E> = {
-  condition: (value: T) => boolean;
-  action: (value: T) => T | E | Promise<T | E>;
+    condition: (value: T) => boolean;
+    action: (value: T) => T | E | Promise<T | E>;
 };
 declare enum MatchMode {
-  FIRST = 0,
-  EVERY = 1,
+    FIRST = 0,
+    EVERY = 1
 }
 type MatchOptions = {
-  continueIfNoMatch?: boolean;
-  continueOnError?: boolean;
-  mode?: MatchMode;
+    continueIfNoMatch?: boolean;
+    continueOnError?: boolean;
+    mode?: MatchMode;
 };
 type NamedMonad<T, E = Error> = {
-  monad: Monad<T, E>;
-  name?: string;
+    monad: Monad<T, E>;
+    name?: string;
 };
 type RetryOptions<E> = {
-  times: number;
-  delay: number;
-  backoffFactor?: number;
-  onError?: (error: E, attempt: number) => void;
-};
-type Cancelable = {
-  cancel: (reason?: string) => void;
+    times: number;
+    delay: number;
+    backoffFactor?: number;
+    onError?: (error: E, attempt: number) => void;
 };
 type FoldResult<U> = {
-  result?: U;
-  error?: Error;
+    result?: U;
+    error?: Error;
 };
-export {
-  Either,
-  Success,
-  Failure,
-  MatchCondition,
-  MatchMode,
-  MatchOptions,
-  NamedMonad,
-  RetryOptions,
-  Cancelable,
-  FoldResult,
-};
+export { Either, Success, Failure, MatchCondition, MatchMode, MatchOptions, NamedMonad, RetryOptions, FoldResult };
 //# sourceMappingURL=util.d.ts.map
